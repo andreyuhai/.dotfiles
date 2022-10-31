90633e134ac312629a390dea0e6dbb6ecc39d2db
andreyuhai <burakaymakci@gmail.com>
2022-11-01 00:03:10 +0100

    Add function to remove whitespace on save

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,1 +1,12 @@
 require("user")
+
+local augroup = vim.api.nvim_create_augroup
+local autocmd = vim.api.nvim_create_autocmd
+
+PreWriteGroup = augroup('PreWriteGroup', {})
+
+autocmd({"BufWritePre"}, {
+    group = PreWriteGroup,
+    pattern = "*",
+    command = "%s/\\s\\+$//e",
+})

67576a143d3dd867282f8878a1b86b03b5a23d0c
Burak <burak.kaymakci@fresha.com>
2023-11-16 11:25:20 +0100

    Add indent-blankline.nvim

--- nvim/.config/nvim/lua/user/packer.lua
+++ nvim/.config/nvim/lua/user/packer.lua
@@ -67,6 +67,8 @@ return require('packer').startup(function(use)
 
   use 'phaazon/hop.nvim'
 
+  use 'lukas-reineke/indent-blankline.nvim'
+
   if packer_bootstrap then
     require('packer').sync()
   end

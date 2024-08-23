eec5a5e374854389b76ea0e9caaa75e65a6f3442
Burak Kaymakci <burak.kaymakci@fresha.com>
2024-08-23 23:04:28 +0200

    More keymaps and stuff for telescope

--- nvim/.config/nvim/lua/plugins/telescope.lua
+++ nvim/.config/nvim/lua/plugins/telescope.lua
@@ -10,29 +10,35 @@ return {
 
       telescope.load_extension("live_grep_args")
 
-      vim.keymap.set("n", "<Leader>ff", function ()
-	builtin.find_files()
-      end)
+      vim.keymap.set("n", "<Leader>fb", builtin.buffers, {})
+      vim.keymap.set("n", "<Leader>ff", builtin.find_files, {})
+      vim.keymap.set("n", "<Leader>fl", telescope.extensions.live_grep_args.live_grep_args, {})
+      vim.keymap.set("n", "<Leader>gf", builtin.git_files, {})
 
-      vim.keymap.set("n", "<Leader>gf", function ()
-	builtin.git_files()
-      end)
+      vim.keymap.set("n", "<Leader>fg", function() builtin.grep_string({ search = vim.fn.input("Grep For > ") }) end, {})
+      vim.keymap.set("n", "<Leader>fw", function() builtin.grep_string { search = vim.fn.expand("<cword>") } end, {})
+    end,
+    opts = function ()
 
-      vim.keymap.set("n", "<Leader>fw", function()
-	builtin.grep_string { search = vim.fn.expand("<cword>") }
-      end)
+      local actions = require("telescope.actions")
+      local lga_actions = require("telescope-live-grep-args.actions")
 
-      vim.keymap.set("n", "<Leader>fb", function()
-	builtin.buffers()
-      end)
-
-      vim.keymap.set("n", "<Leader>fg", function()
-	builtin.grep_string({ search = vim.fn.input("Grep For > ") })
-      end)
-
-      vim.keymap.set("n", "<Leader>fl", function()
-	telescope.extensions.live_grep_args.live_grep_args()
-      end)
+      return {
+	extensions = {
+	  live_grep_args = {
+	    auto_quoting = true,
+	    mappings = {
+	      i = {
+		["<C-k>"] = lga_actions.quote_prompt(),
+		["<C-i>"] = lga_actions.quote_prompt({ postfix = " --iglob "}),
+		["<C-t>"] = lga_actions.quote_prompt({ postfix = " -g "}),
+		-- freeze the current list and start a fuzzy search in the frozen list
+		["<C-r>"] = actions.to_fuzzy_refine,
+	      }
+	    }
+	  }
+	}
+      }
     end
   }
 }
